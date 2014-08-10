3e961daf21a9e93d5b24f930381ac24e7cd8808b
Konrad Klimaszewski <graag@o2.pl>
2014-08-10 16:12:44 +0200

    Implementation of CPU fan speed adjustment.

--- README.md
+++ README.md
@@ -10,6 +10,19 @@ This script must be run with root or sudo privileges. Only Celsius temperatures 
 
     sudo ./temp_throttle 80
 
+The script can in addition adjust CPU fan speed. When a temperature threshold is exceeded the CPU fan speed is set to maximum. When CPU cools down the fan speed is reset to the previous value.
+
+Configuration of the script can be altered using $HOME/.config/temp-throttle settings file. Supported settings:
+# File defining available CPU frequencies 
+FREQ_FILE=/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies
+# Files defining available frequency range. Used when FREQ_FILE is not available.
+FREQ_MIN=/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq
+FREQ_MAX=/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq
+# Temperature sensor
+TEMP_FILE=/sys/class/thermal/thermal_zone0/temp
+# CPU fan device
+COOLING_DEVICE=/sys/class/thermal/cooling_device0
+
 
 For more instructions, see here:  
 http://seperohacker.blogspot.com/2012/10/linux-keep-your-cpu-cool-with-frequency.html
@@ -22,5 +35,6 @@ Links: http://seperohacker.blogspot.com/2012/10/linux-keep-your-cpu-cool-with-fr
 
 License: GNU GPL 2.0
 
-Usage: `temp_throttle.sh max_temp`  
+Usage CPU throttle:                        `temp_throttle.sh max_temp`  
+Usage CPU throttle + fan speed adjustment: `temp_throttle.sh max_temp max_fan_temp`
 USE CELSIUS TEMPERATURES
--- temp_throttle.sh
+++ temp_throttle.sh
@@ -25,21 +25,46 @@ err_exit () {
 	exit 128
 }
 
-if [ $# -ne 1 ]; then
+if [ "$#" -ne "1" ] && [ "$#" -ne "2" ]; then
+	echo $#
 	# If temperature wasn't given, then print a message and exit.
-	echo "Please supply a maximum desired temperature in Celsius." 1>&2
-	echo "For example:  ${0} 60" 1>&2
+	echo "${0} TT [FT]" 1>&2
+	echo "" 1>&2
+	echo "Manage CPU throttle and CPU fan speed based on CPU temperature." 1>&2
+	echo "" 1>&2
+	echo "Parameters:" 1>&2
+	echo " TT - Throttle Threshold: " 1>&2
+	echo "      maximum desired temperature in Celsius above which CPU is throtteled." 1>&2
+	echo " FT - Fan Threshold:" 1>&2
+	echo "      maximum desired temperature in Celsius above which CPU Fan speed is set to maximum." 1>&2
+	echo "" 1>&2
+	echo "For example:" 1>&2
+	echo " ${0} 60" 1>&2
+	echo " ${0} 70 60" 1>&2
+	echo "" 1>&2
+	echo "Configuration:" 1>&2
+	echo "${0} can be customized via settings stored in $HOME/.config/temp-throttle" 1>&2
 	exit 2
 else
 	#Set the first argument as the maximum desired temperature.
 	MAX_TEMP=$1
+	MAX_FAN_TEMP=999
+	if [ $# -eq 2 ]; then
+		MAX_FAN_TEMP=$2
+	fi
 fi
 
 
 ### START Initialize Global variables.
 
+# Load user config
+if [ -f $HOME/.config/temp-throttle ]; then
+	. $HOME/.config/temp-throttle
+fi
+
 # The frequency will increase when low temperature is reached.
 LOW_TEMP=$((MAX_TEMP - 5))
+LOW_FAN_TEMP=$((MAX_FAN_TEMP - 5))
 
 CORES=$(nproc) # Get number of CPU cores.
 echo -e "Number of CPU cores detected: $CORES\n"
@@ -48,10 +73,12 @@ CORES=$((CORES - 1)) # Subtract 1 from $CORES for easier counting later.
 # Temperatures internally are calculated to the thousandth.
 MAX_TEMP=${MAX_TEMP}000
 LOW_TEMP=${LOW_TEMP}000
+MAX_FAN_TEMP=${MAX_FAN_TEMP}000
+LOW_FAN_TEMP=${LOW_FAN_TEMP}000
 
-FREQ_FILE="/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies"
-FREQ_MIN="/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq"
-FREQ_MAX="/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq"
+FREQ_FILE=${FREQ_FILE:-/sys/devices/system/cpu/cpu0/cpufreq/scaling_available_frequencies}
+FREQ_MIN=${FREQ_MIN:-/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_min_freq}
+FREQ_MAX=${FREQ_MAX:-/sys/devices/system/cpu/cpu0/cpufreq/cpuinfo_max_freq}
 
 # Store available cpu frequencies in a space separated string FREQ_LIST.
 if [ -f $FREQ_FILE ]; then
@@ -83,12 +110,16 @@ TEMPERATURE_FILES="
 null
 "
 
+COOLING_DEVICE=${COOLING_DEVICE:-/sys/class/thermal/cooling_device0}
+
 # Store the first temperature location that exists in the variable TEMP_FILE.
 # The location stored in $TEMP_FILE will be used for temperature readings.
-for file in $TEMPERATURE_FILES; do
-	TEMP_FILE=$file
-	[ -f $TEMP_FILE ] && break
-done
+if [ -z $TEMP_FILE ]; then
+	for file in $TEMPERATURE_FILES; do
+		TEMP_FILE=$file
+		[ -f $TEMP_FILE ] && break
+	done
+fi
 
 [ $TEMP_FILE == "null" ] && err_exit "The location for temperature reading was not found."
 
@@ -126,22 +157,47 @@ unthrottle () {
 	fi
 }
 
+# Will speedup fans.
+throttle_fan () {
+	echo "speedup fan"
+	# Store current fan speed
+	FAN_STATE=$(cat $COOLING_DEVICE/cur_state)
+	echo $(cat $COOLING_DEVICE/max_state) > $COOLING_DEVICE/cur_state
+}
+
+# Will slow down fans.
+unthrottle_fan () {
+	echo "slowdown fan"
+	# Restore fan speed
+	echo $FAN_STATE > $COOLING_DEVICE/cur_state
+}
+
 get_temp () {
 	# Get the system temperature.
-	
+
+	LAST_TEMP=$TEMP	
 	TEMP=$(cat $TEMP_FILE)
 }
 
 ### END define script functions.
 
+TEMP=0
+FAN_STATE=0
 
 # Mainloop
 while true; do
 	get_temp # Gets the current tempurature and set it to the variable TEMP.
+
 	if   [ $TEMP -gt $MAX_TEMP ]; then # Throttle if too hot.
 		throttle
 	elif [ $TEMP -le $LOW_TEMP ]; then # Unthrottle if cool.
 		unthrottle
+	fi
+
+	if   [ $TEMP -gt $MAX_FAN_TEMP ] && [ $LAST_TEMP -le $MAX_FAN_TEMP ]; then # Speed up fan if too hot.
+		throttle_fan
+	elif [ $TEMP -le $LOW_FAN_TEMP ] && [ $LAST_TEMP -gt $LOW_FAN_TEMP ]; then # Slow down fan if cool.
+		unthrottle_fan
 	fi
 	sleep 3 # The amount of time between checking tempuratures.
 done
